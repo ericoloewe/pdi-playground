f3607c3b7aa3a23734537f214b6773bdb94fcaf4
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-08-31 20:20:18 -0300

    refactor(vendors): improved vendors.

--- tasks/gulp/scripts.js
+++ tasks/gulp/scripts.js
@@ -7,18 +7,28 @@ var tsProject = ts.createProject("tsconfig.json");
 
 var paths = {
     compile: ["./app/scripts/**/*.ts"],
-    watch: ["./app/scripts/**/*.ts"],
+    vendors: ["./app/scripts/extensions/**/*.js", "!./app/scripts/extensions/jquery/*.js"],
+    watch: ["./app/scripts/**/*.ts", "./app/scripts/**/*.js"],
     dest: "./app/scripts"
 };
 
 var resourcesPaths = {
     js: ["./resources/imports.js", "./main.js"],
     compile: ["./resources/**/*.ts"],
-    watch: ["./resources/**/*.ts"],
+    watch: ["./resources/**/*.ts", "./resources/**/*.js"],
     dest: "./"
 };
 
-gulp.task("scripts", function () {
+gulp.task("scripts:vendors", function () {
+    return gulp.src(paths.vendors)
+        .pipe(concat("vendors.js"))
+        .pipe(gulp.dest(paths.dest))
+        .pipe(rename({ suffix: ".min" }))        
+        .pipe(uglify())
+        .pipe(gulp.dest(paths.dest));
+});
+
+gulp.task("scripts", ["scripts:vendors"], function () {
     return gulp.src(paths.compile)
         .pipe(ts({
             noImplicitAny: true,
