105d7866ba60816eebd77f84e6b8cd4c4c3fd1ec
Érico de Souza Loewe <ed-lowe@hotmail.com>
2016-08-31 20:10:32 -0300

    feat(Application): add application.

--- app/scripts/app.ts
+++ app/scripts/app.ts
@@ -1,18 +1,31 @@
-/// <reference path="views/statisticsView.ts" />
-/// <reference path="config/fragments.ts" />
+/// <reference path="config/application.ts" />
 
-var view = new StatisticsView();
+class PDIPlayGroundApplication extends Application {
+    constructor() {
+        super();
+        this.createViews();
+        this.createFragments();
+    }
 
-view.render();
+    private createViews() {
+        this.views = [
+            new StatisticsView()
+        ];
+    }
 
-var fragments = new Fragments([
-    new Fragment("home", "views/home.html"),
-    new Fragment("statistics", "views/statistics.html", [
-        new Fragment("color-gray", "views/statistics/color-gray.html"),
-        new Fragment("color-red", "views/statistics/color-red.html"),
-        new Fragment("color-green", "views/statistics/color-green.html"),
-        new Fragment("color-blue", "views/statistics/color-blue.html"),
-        new Fragment("color-alpha", "views/statistics/color-alpha.html")
-    ]),
-    new Fragment("header", "views/shared/header.html")
-]);
+    private createFragments() {
+        this.fragments = new Fragments([
+            new Fragment("home", "views/home.html"),
+            new Fragment("statistics", "views/statistics.html", [
+                new Fragment("color-gray", "views/statistics/color-gray.html"),
+                new Fragment("color-red", "views/statistics/color-red.html"),
+                new Fragment("color-green", "views/statistics/color-green.html"),
+                new Fragment("color-blue", "views/statistics/color-blue.html"),
+                new Fragment("color-alpha", "views/statistics/color-alpha.html")
+            ]),
+            new Fragment("header", "views/shared/header.html")
+        ]);
+    }
+}
+
+var app = new PDIPlayGroundApplication();
---
+++ app/scripts/config/application.ts
@@ -0,0 +1,11 @@
+/// <reference path="../views/statisticsView.ts" />
+/// <reference path="../config/fragments.ts" />
+
+class Application {
+    protected views: Array<View>;
+    protected fragments: Fragments;
+
+    constructor() {
+        
+    }
+}
